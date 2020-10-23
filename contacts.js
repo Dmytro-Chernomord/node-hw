2ebc7207d1b34c09de6a87d09f2ab563de977e2b
Dmytro <tschernomord@gmail.com>
2020-10-23 16:46:56 +0200

    remove emty lines

--- contacts.js
+++ contacts.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
 const path = require('path')
 const nanoid = require('nanoid')
-
-
 const contactsPath = path.join(__dirname, './db/contacts.json')
 function listContacts() {
     fs.readFile(contactsPath, "utf-8", (err, data) => {
@@ -15,7 +13,6 @@ function getContactById(contactId) {
         const foundContacts = parseData.filter(el => el.id === contactId)
         console.log(foundContacts);
     })
-
 }
 function removeContact(contactId) {
     fs.readFile(contactsPath, "utf-8", (err, data) => {
@@ -25,17 +22,12 @@ function removeContact(contactId) {
         fs.writeFile(contactsPath, strNewData, err => {
             if (err) {
                 console.log(err);
-
             }
         })
-
     })
 }
-
 function addContact(name, email, phone) {
     const id = nanoid.nanoid(3);
-
-
     const newContact = {
         id,
         name,
@@ -49,14 +41,10 @@ function addContact(name, email, phone) {
         fs.writeFile(contactsPath, strNewData, err => {
             if (err) {
                 console.log(err);
-
             }
         })
-
     })
 }
-
-
 module.exports = {
     getContactById,
     removeContact, addContact, listContacts
