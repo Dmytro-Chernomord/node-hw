eb008fec887203a707ac719cc0c79784e5348f5b
Dmytro <tschernomord@gmail.com>
2020-11-16 14:51:09 +0100

    Update auth.router.js

--- src/auth/auth.router.js
+++ src/auth/auth.router.js
@@ -4,7 +4,9 @@ const { validate } = require('../helpers/validate.middleware');
 const { createUserSchema, loginSchema } = require('../user/user.schemes');
 const { register, login, logout } = require('./auth.controller');
 const router = Router()
+
 router.post('/register', validate(createUserSchema), register)
 router.post('/login', validate(loginSchema), login)
 router.post('/logout', authorize, logout)
+
 exports.routerAuth = router
