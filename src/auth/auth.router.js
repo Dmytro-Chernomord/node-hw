25a6289c5fea14e23a4eea8676d10c39da62f135
Dmytro <tschernomord@gmail.com>
2020-11-17 13:34:38 +0100

    all done

--- src/auth/auth.router.js
+++ src/auth/auth.router.js
@@ -1,13 +1,15 @@
 const { Router } = require('express');
 const { authorize } = require('../helpers/authorize.middleware');
+const { checkVerification } = require('../helpers/emailSender');
 const { imageCreator } = require('../helpers/imageCreator');
 const { validate } = require('../helpers/validate.middleware');
 const { createUserSchema, loginSchema } = require('../user/user.schemes');
-const { register, login, logout, avatar } = require('./auth.controller');
+const { register, login, logout } = require('./auth.controller');
 const router = Router()
 
 router.post('/register', validate(createUserSchema), register)
 router.post('/login', validate(loginSchema), login)
 router.post('/logout', authorize, logout)
+router.get('/verify/:verificationToken', checkVerification);
 
 exports.routerAuth = router
